9b319dbdfccadecf24b372d3ccf86bf195fa7a80
kevin pernet <hoxx@kevins-macbook-pro.home>
2023-06-13 18:22:44 +0200

    first commit

---
+++ database/models/ArticleDB.php
@@ -0,0 +1,90 @@
+<?php
+require_once __DIR__ . '/../database.php';
+
+class ArticleDB
+{
+  function __construct(private PDO $pdo)
+  {
+  }
+  public function fetchAll(): array
+  {
+    $statement = $this->pdo->prepare('SELECT article.*, user.firstname, user.lastname FROM article LEFT JOIN user ON article.author = user.id');
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+
+  public function fetchOne(string $id): array
+  {
+    $statement = $this->pdo->prepare('SELECT article.*, user.firstname, user.lastname FROM article LEFT JOIN user ON article.author = user.id WHERE article.id=:id');
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+    return $statement->fetch();
+  }
+
+  public function deleteOne(string $id): string
+  {
+    $statement = $this->pdo->prepare('DELETE FROM article WHERE id=:id');
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+    return $id;
+  }
+
+  public function createOne($article): array
+  {
+    $statement = $this->pdo->prepare('
+            INSERT INTO article (
+                title,
+                category,
+                content,
+                image,
+                author
+            ) VALUES (
+                :title,
+                :category,
+                :content,
+                :image,
+                :author
+            )
+        ');
+    $statement->bindValue(':title', $article['title']);
+    $statement->bindValue(':content', $article['content']);
+    $statement->bindValue(':category', $article['category']);
+    $statement->bindValue(':image', $article['image']);
+    $statement->bindValue(':author', $article['author']);
+    $statement->execute();
+    return $this->fetchOne($this->pdo->lastInsertId());
+  }
+
+  public function updateOne($article): array
+  {
+    $statement = $this->pdo->prepare('
+            UPDATE article
+            SET
+                title=:title,
+                category=:category,
+                content=:content,
+                image=:image,
+                author=:author
+            WHERE id=:id
+        ');
+    $statement->bindValue(':title', $article['title']);
+    $statement->bindValue(':content', $article['content']);
+    $statement->bindValue(':category', $article['category']);
+    $statement->bindValue(':image', $article['image']);
+    $statement->bindValue(':id', $article['id']);
+    $statement->bindValue(':author', $article['author']);
+    $statement->execute();
+    return $article;
+  }
+
+  public function fetchUserArticle(string $authorId): array
+  {
+    $statement = $this->pdo->prepare('SELECT * FROM article WHERE author=:authorId');
+    $statement->bindValue(':authorId', $authorId);
+    $statement->execute();
+    return $statement->fetchAll();
+  }
+}
+
+
+return new ArticleDB($pdo);
---
+++ database/security.php
@@ -0,0 +1,79 @@
+<?php
+require_once __DIR__ . '/database.php';
+class AuthDB
+{
+
+  public function __construct(private PDO $pdo)
+  {
+  }
+
+  function login(string $userId): void
+  {
+    $statement = $this->pdo->prepare('INSERT INTO session VALUES (
+            :sessionid,
+            :userid
+        )');
+    $sessionId = bin2hex(random_bytes(32));
+    $statement->bindValue(':userid', $userId);
+    $statement->bindValue(':sessionid', $sessionId);
+    $statement->execute();
+    $signature = hash_hmac('sha256', $sessionId, '4cd30a3e9bd36ae867730f712e15b4d29d0473916d5d61e8425346f277c63cf9');
+    setcookie('session', $sessionId, time() + 60 * 60 * 24 * 14, '', '', false, true);
+    setcookie('signature', $signature, time() + 60 * 60 * 24 * 14, "", "", false, true);
+    return;
+  }
+
+  function register(array $user): void
+  {
+    $statement = $this->pdo->prepare('INSERT INTO user VALUES (
+            DEFAULT,
+            :firstname,
+            :lastname,
+            :email,
+            :password
+        )');
+    $hashedPassword = password_hash($user['password'], PASSWORD_ARGON2I);
+    $statement->bindValue(':firstname', $user['firstname']);
+    $statement->bindValue(':lastname', $user['lastname']);
+    $statement->bindValue(':email', $user['email']);
+    $statement->bindValue(':password', $hashedPassword);
+    $statement->execute();
+    return;
+  }
+
+  function isLoggedin(): array | false
+  {
+    $sessionId = $_COOKIE['session'] ?? '';
+    $signature = $_COOKIE['signature'] ?? '';
+    if ($sessionId && $signature) {
+      $hash = hash_hmac('sha256', $sessionId, '4cd30a3e9bd36ae867730f712e15b4d29d0473916d5d61e8425346f277c63cf9');
+      if (hash_equals($hash, $signature)) {
+        $statement = $this->pdo->prepare('SELECT * FROM session JOIN user on user.id=session.userid WHERE session.id=:sessionid');
+        $statement->bindValue(':sessionid', $sessionId);
+        $statement->execute();
+        $user = $statement->fetch();
+      }
+    }
+    return $user ?? false;
+  }
+
+  function logout(string $sessionId): void
+  {
+    $statement = $this->pdo->prepare('DELETE FROM session WHERE id=:id');
+    $statement->bindValue(':id', $sessionId);
+    $statement->execute();
+    setcookie('session', '', time() - 1);
+    setcookie('signature', '', time() - 1);
+    return;
+  }
+
+  function getUserFromEmail(string $email): array | false
+  {
+    $statement = $this->pdo->prepare('SELECT * FROM user WHERE email=:email');
+    $statement->bindValue(':email', $email);
+    $statement->execute();
+    return $statement->fetch();
+  }
+}
+
+return new AuthDB($pdo);
